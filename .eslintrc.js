92d399a14abf51ac9f08acffa15fbffd30d505aa
senner <vadim.senner@gmail.com>
2022-10-25 20:06:42 +0300

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: [
+    "plugin:sonarjs/recommended",
+    "prettier"
+  ],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+  },
+};
